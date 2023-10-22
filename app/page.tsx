42f574c6ea0e421f718ffd0f817c1a05b26692c3
alessandrocambruzzi <146895401+alessandrocambruzzi@users.noreply.github.com>
2023-10-22 20:17:50 +0000

    fix: import Image from 'next/image';

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Trending from  "./components/Trending";
 import Latest from "./components/Latest";
+import Image from 'next/image';
 
 
 export default function Home() {

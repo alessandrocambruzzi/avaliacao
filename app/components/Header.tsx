3e712a936247b642a484fcda9faaf25d93ee64bb
alessandrocambruzzi <146895401+alessandrocambruzzi@users.noreply.github.com>
2023-10-22 20:14:05 +0000

    fix: add footer

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,9 @@
 export default function Header() {
-    return (
-      <header className="text-center bg-white">
-        <p className="w-116 h-43 font-playfair font-extrabold text-4xl leading-43 flex items-center text-[#0F172A]">
-          UTFPR<span className="text-red-500">.</span>
-        </p>
-      </header>
-    );
-  }
-  
+  return (
+    <header className="text-center bg-white">
+      <p className="w-116 h-43 font-playfair font-extrabold text-4xl leading-43 mx-auto flex items-center text-[#0F172A]">
+        UTFPR<span className="text-red-500">.</span>
+      </p>
+    </header>
+  );
+}
